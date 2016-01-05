bb11665bd41787ab78ac02c39e735b43eb64a686
Jose Pedro de Sousa Martins <zpedro.martins@gmail.com>
2016-01-05 00:45:53 +0000

    Tests Added.

--- pa.iscde.testextension/src/pa/iscde/testextension/JavaDocFieldRenderPostVisit.java
+++ pa.iscde.testextension/src/pa/iscde/testextension/JavaDocFieldRenderPostVisit.java
@@ -1,6 +1,7 @@
 package pa.iscde.testextension;
 
 import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 
 import pa.iscde.javadoc.export.render.JavaDocFieldRender;
 
@@ -8,7 +9,12 @@ public class JavaDocFieldRenderPostVisit implements JavaDocFieldRender {
 
     @Override
     public boolean render(FieldDeclaration field, StringBuilder sb) {
-	// TODO Auto-generated method stub
+	sb.append("<br>");
+	sb.append("<hr>");
+	sb.append("Extension:").append(this.getClass().getSimpleName()).append("<br>");
+	sb.append("FieldName:").append( ((VariableDeclarationFragment)field.fragments().get(0)).getName().getFullyQualifiedName()).append("<br>");
+	sb.append("<hr>");
+	sb.append("<br>");
 	return false;
     }
 
--- pa.iscde.testextension/src/pa/iscde/testextension/JavaDocNamedMethodRenderPostVisit.java
+++ pa.iscde.testextension/src/pa/iscde/testextension/JavaDocNamedMethodRenderPostVisit.java
@@ -7,8 +7,13 @@ import pa.iscde.javadoc.export.render.JavaDocMethodRender;
 public class JavaDocNamedMethodRenderPostVisit implements JavaDocMethodRender {
 
     @Override
-    public boolean render(MethodDeclaration field, StringBuilder sb) {
-	// TODO Auto-generated method stub
+    public boolean render(MethodDeclaration method, StringBuilder sb) {
+	sb.append("<br>");
+	sb.append("<hr>");
+	sb.append("Extension:").append(this.getClass().getSimpleName()).append("<br>");
+	sb.append("MethodName:").append(method.getName().getFullyQualifiedName() ).append("<br>");
+	sb.append("<hr>");
+	sb.append("<br>");
 	return false;
     }
 
